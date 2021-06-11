67768904a789be948a9a7a4157ea41d53b09644a
Blanka <blankafogarasi@gmail.com>
2021-06-11 23:09:17 +0200

    add token from localstorage

--- src/services/hangman.service.js
+++ src/services/hangman.service.js
@@ -3,7 +3,14 @@ export const hangmanService = {
 };
 
 async function initializeWordsToGuess(gameId) {
-    const response = await fetch(`${process.env.REACT_APP_API_URL}/hangman/get_words?id=${gameId}`)
+    const token = localStorage.getItem('token');
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/hangman/get_words?id=${gameId}`, {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json',
+            'patatas-fritas-token': token
+        }
+    })
 
     return await response.json()
     // const words = [
