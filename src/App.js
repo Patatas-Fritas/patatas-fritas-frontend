54eff2a6e078ead242da25847100226524a02a5a
Blanka <blankafogarasi@gmail.com>
2021-06-11 21:41:28 +0200

    add material ui theme

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { ChuckNorrisApi } from './pages/ChuckNorrisApi/ChuckNorrisApi.js'
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {ChuckNorrisApi} from './pages/ChuckNorrisApi/ChuckNorrisApi.js'
 import Login from './components/Login/Login';
 import Registration from './components/Registration/Registration';
 import HangmanPage from "./pages/Games/HangmanPage/HangmanPage";
 import HotspotPage from "./pages/Games/HotspotPage/HotspotPage";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import {createMuiTheme, ThemeProvider} from "@material-ui/core";
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#5C9129'
+            dark: '#538723',
+            main: '#5C9129',
+            light: '#8BBF58',
+            lightest: '#B2C7A3'
+        },
+        secondary: {
+            main: '#993166'
         }
+    },
+    typography: {
+        fontFamily: 'Lato',
+        fontWeightRegular: 400,
+        fontWeightBold: 700
     }
 })
 
 function App() {
-  return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/chuck" component={ChuckNorrisApi} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Registration} />
-            <Route exact path="/hangman" component={HangmanPage} />
-            <Route exact path="/admin/hotspot" component={HotspotPage} />
-          </Switch>
-        </Router>
-      </div>
-  );
+    return (
+        <div className="App">
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <Switch>
+                        <Route exact path="/chuck" component={ChuckNorrisApi}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/register" component={Registration}/>
+                        <Route exact path="/hangman" component={HangmanPage}/>
+                        <Route exact path="/admin/hotspot" component={HotspotPage}/>
+                    </Switch>
+                </Router>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
