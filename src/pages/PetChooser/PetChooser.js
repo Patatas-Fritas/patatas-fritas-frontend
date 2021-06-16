afc407b012d53e14c0f40ff4236105d3c55a1a2f
blankafogarasi <blankafogarasi@gmail.com>
2021-06-16 19:06:28 +0200

    pet redirection

--- src/pages/PetChooser/PetChooser.js
+++ src/pages/PetChooser/PetChooser.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {makeStyles} from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
@@ -40,6 +41,7 @@ function PetChooser() {
 
   const [errorMessage, setErrorMessage] = useState('');
   const classes = useStyles();
+  const history = useHistory();
 
   const savePet = async () => {
     console.log('petchooser request sent')
@@ -58,6 +60,7 @@ function PetChooser() {
     })
     console.log(response.status)
     console.log(response)
+    history.push('/petfeeder')
   };
 
   const onPetNameChange = (event) => {
